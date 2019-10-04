1bb2cb44c7b71617684d36ea86109b924f196951
Alexander Thomas <cawfree@Alexanders-MacBook-Pro.local>
2019-10-04 13:31:58 +0100

    Published to GitHub! 🚀

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # react-native-view-evaluator
 A React Native &lt;Component /> which can be used to determine the post-layout configuration of a &lt;View />.
+
+## What's the point?
+Sometimes in React Native, it's important to know the dimensions of the content you're going to render, before you actually render it! Take the [`<FlatList/>`](https://facebook.github.io/react-native/docs/flatlist.html); it's important to know your `itemWidth` before you render the content, and it's impossibe to use layout `flex` to fill content; so a `<FlatList/>` which resizes to fill your content is out of the question. Until now at least.
+
+## 🚀 Getting Started
+
+Using [`npm`]():
+
+```sh
+npm install --save react-native-view-evaluator
+```
+
+Using [`yarn`]():
+
+```sh
+yarn add react-native-view-evaluator
+```
+
+## ✍️ Example
+
+```javascript
+import React from 'react';
+import { FlatList, View } from 'react-native';
+
+export default () => (
+  <ViewEvaluator
+    style={{
+      width: 100,
+      height: null,
+    }}
+    renderEvaluated={(arrayOfChildren, layouts) => {
+      const maxHeight = layouts
+        .reduce(
+          (v, { height }) => (
+            Math.max(
+              v,
+              height,
+            )
+          ),
+          Number.MIN_VALUE,
+        );
+      return (
+        <FlatList
+          renderItem={({ item: viewToRender, index }) => (
+            <View
+              key={index}
+              style={{
+                width: 100,
+                height: maxHeight,
+              }}
+            >
+              {viewToRender}
+            </View>
+          )}
+          data={arrayOfChildren}
+        />
+      );
+    }}
+  >
+    <Text
+      numberOfLines={2}
+    >
+      {'There\'s no way to tell how big this content will be until we actually render it, you know?'}
+    </View>
+    <Text
+      numberOfLines={1}
+    >
+      {'So, if you wanted a <FlatList/> to stretch to fill these elements, you\'d be a bit stuck.'}
+    </View>
+  <View>
+);
+```
+
+As you can see, once the size of each `<View />` has been calculated, they each get passed as an array to the `renderEvaluated` method, alongside a matching array of the resultan layouts.
+
+
+## ⚠️ Caveats
+This is only really designed for simple components that do not do anything intense, or stateful with respect to your application state, during `componentDidMount`, i.e. an API call. This is because the components are effectively rendered first, before being passed into the `renderEvaluated` callback. You won't see the components during this operation, but they _will_ act as if they are visible.
+
+## ✌️ License
+[MIT](https://opensource.org/licenses/MIT)
+
---
+++ index.js
@@ -0,0 +1,157 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Alert, View, StyleSheet, ActivityIndicator } from 'react-native';
+
+class ViewEvaluator extends React.Component {
+  static hasLayouts = (layouts = []) => (
+    layouts
+      .reduce(
+        (r, e) => (r && !!e),
+        true,
+      )
+  );
+  constructor(props) {
+    super(props);
+    const { children } = props;
+    this.state = {
+      children: [],
+      layouts: [],
+      evaluating: React.Children.count(
+        children,
+      ) > 0,
+    };
+  }
+  createMeasurementContainer = (onLayout, child) => (
+    <View
+      onLayout={onLayout}
+      removeClippedSubviews={false}
+      collapsible={false}
+      renderToHardwareTextureAndroid
+    >
+      {child}
+    </View>
+  );
+  onLayoutsAccumulated = (layouts = []) => this.setState(
+    {
+      evaluating: false,
+    },
+  );
+  shouldEvaluate = children => this.setState(
+    {
+      layouts: [
+        ...Array(
+          React.Children.count(children),
+        ),
+      ],
+      children: React
+        .Children
+        .map(
+          children,
+          (child, i) => (
+            this
+              .createMeasurementContainer(
+                ({ nativeEvent: { layout } }) => {
+                  const { layouts } = this.state;
+                  layouts[i] = layout;
+                  if (ViewEvaluator.hasLayouts(layouts)) {
+                    const { onLayoutsAccumulated } = this;
+                    return onLayoutsAccumulated(
+                      layouts,
+                    );
+                  }
+                  return null;
+                },
+                child,
+              )
+          ),
+        ),
+    },
+  );
+  componentDidMount() {
+    const { evaluating } = this.state;
+    if (evaluating) {
+      const { children } = this.props;
+      return this.shouldEvaluate(
+        children,
+      );
+    }
+    return null;
+  }
+  render() {
+    const {
+      children,
+      renderEvaluating,
+      renderEvaluated,
+      ...extraProps
+    } = this.props;
+    const {
+      children: toBeEvaluated,
+      layouts,
+      evaluating,
+    } = this.state;
+    return (
+      <View
+        {...extraProps}
+      >
+        <View
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              opacity: 0,
+            },
+          ]}
+        >
+          {toBeEvaluated}
+        </View>
+        {(!!evaluating) && (
+          renderEvaluating()
+        )}
+        {(!evaluating) && (
+          renderEvaluated(
+            React.Children.toArray(
+              children,
+            ),
+            layouts,
+          )
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet
+  .create(
+    {
+      evaluating: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    },
+  );
+
+ViewEvaluator.propTypes = {
+  ...View.propTypes,
+  renderEvaluating: PropTypes.func,
+  renderEvaluated: PropTypes.func,
+};
+
+ViewEvaluator.defaultProps = {
+  ...View.defaultProps,
+  renderEvaluating: () => (
+    <View
+      style={styles.evaluating}
+    >
+      <ActivityIndicator
+      />
+    </View>
+  ),
+  renderEvaluated: (arrayOfChildren, layouts) => (
+    <React.Fragment
+    >
+      {arrayOfChildren}
+    </React.Fragment>
+  ),
+};
+
+export default ViewEvaluator;
